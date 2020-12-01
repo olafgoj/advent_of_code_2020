e3e8358bf2cbd2f6422317e5558e12afcebce510
Olaf Goj <olaf@goj.email>
2020-12-01 21:56:22 +0100

    Add solution for day 1

---
+++ src/day1_2.rb
@@ -0,0 +1,9 @@
+input = File.readlines('../res/day1.input').map(&:to_i)
+
+output = input
+         .combination(3)
+         .select { |x, y, z| x + y + z == 2020 }
+         .flatten
+         .inject(:*)
+
+puts output
